6df24bae613edbd19150c74178869ead75d47ea4
Tolu Olugbesan <tolugbesan@gmail.com>
2024-03-07 17:54:23 +0100

    Fix carousel scrolling issue

--- apps/web/src/components/TopMusicians.tsx
+++ apps/web/src/components/TopMusicians.tsx
@@ -20,7 +20,7 @@ import Link from "next/link";
 
 function RankBadge({ rank }: { rank: number }) {
   return (
-    <div className="w-6 h-6 flex items-center justify-center rounded-full transition-all duration-200 bg-neutral-800 md:bg-black/20 group-hover:bg-neutral-800 text-xs text-white font-federant">
+    <div className="flex h-6 w-6 items-center justify-center rounded-full bg-neutral-800 font-federant text-xs text-white transition-all duration-200 group-hover:bg-neutral-800 md:bg-black/20">
       {rank}
     </div>
   );
@@ -32,19 +32,19 @@ type ProfileCardProps = {
 
 function ProfileCard({ rank }: ProfileCardProps) {
   return (
-    <Card className="w-full h-full flex flex-col justify-between bg-black/15 border border-primary/20 overflow-hidden outline-none transition-all duration-200 cursor-pointer group md:hover:bg-primary/10 md:hover:outline md:hover:outline-primary/20 md:hover:outline-offset-2">
-      <CardContent className="relative flex justify-center items-center p-0 md:p-4">
+    <Card className="group flex h-full w-full cursor-pointer flex-col justify-between overflow-hidden border border-primary/20 bg-black/15 outline-none transition-all duration-200 md:hover:bg-primary/10 md:hover:outline md:hover:outline-offset-2 md:hover:outline-primary/20">
+      <CardContent className="relative flex items-center justify-center p-0 md:p-4">
         <div className="absolute inset-2 z-10">
           <RankBadge rank={rank} />
         </div>
-        <Avatar className="rounded-none w-[302px] h-[302px] md:w-24 md:h-24 md:rounded-full">
+        <Avatar className="h-full w-full rounded-none md:h-24 md:w-24 md:rounded-full">
           <AvatarImage
-            className="w-full h-full"
+            className="h-full w-full"
             src={`https://i.pravatar.cc/300?img=${rank}`}
           />
           <AvatarFallback className="relative bg-black/5">
             <svg
-              className="absolute w-full h-full text-black/20 left-1/2 -translate-x-1/2"
+              className="absolute left-1/2 h-full w-full -translate-x-1/2 text-black/20"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -58,19 +58,19 @@ function ProfileCard({ rank }: ProfileCardProps) {
           </AvatarFallback>
         </Avatar>
       </CardContent>
-      <CardHeader className="md:text-center text-sm p-4 md:p-0 md:pb-4">
+      <CardHeader className="p-4 text-sm md:p-0 md:pb-4 md:text-center">
         <CardTitle>Name {rank}</CardTitle>
         <CardDescription>
           <span>Token Price: </span>
 
-          <span className="text-white font-federant">
+          <span className="font-federant text-white">
             {(rank / 3).toFixed(2)}
             <span className="tracking-widest"> ETP</span>
           </span>
         </CardDescription>
       </CardHeader>
       <CardFooter className="flex flex-col items-start justify-end md:hidden">
-        <span className="h-1 w-full bg-card/50 rounded-full" />
+        <span className="h-1 w-full rounded-full bg-card/50" />
       </CardFooter>
     </Card>
   );
@@ -98,7 +98,7 @@ function TopMusicians() {
 
   return (
     <section>
-      <div className="container flex justify-between items-center md:px-24">
+      <div className="container flex items-center justify-between md:px-24">
         <div className="flex flex-col gap-2">
           <h2 className="text-3xl font-semibold">
             Top Musicians{" "}
@@ -114,20 +114,20 @@ function TopMusicians() {
           asChild
         >
           <Link href="/charts">
-            <BsBarChart className="w-[13px] h-[13px]" />
+            <BsBarChart className="h-[13px] w-[13px]" />
             View Charts
           </Link>
         </Button>
       </div>
       <Spacer size={40} />
       <div className="container flex justify-center">
-        <div className="hidden md:grid md:grid-cols-[repeat(3,minmax(180px,180px))] lg:grid-cols-[repeat(4,minmax(180px,180px))] gap-5">
+        <div className="hidden gap-5 md:grid md:grid-cols-[repeat(3,minmax(180px,180px))] lg:grid-cols-[repeat(4,minmax(180px,180px))]">
           {[...Array(12)].map((_, i) => (
             <ProfileCard key={i} rank={i + 1} />
           ))}
         </div>
       </div>
-      <div className="flex flex-col items-center gap-6 scroll-smooth overflow-hidden md:hidden">
+      <div className="flex flex-col items-center gap-6 overflow-hidden scroll-smooth md:hidden">
         <Carousel
           opts={{
             align: "start",
@@ -136,7 +136,7 @@ function TopMusicians() {
         >
           <CarouselContent>
             {[...Array(6)].map((_, i) => (
-              <CarouselItem className="max-w-xs aspect-[320/404]" key={i}>
+              <CarouselItem className="aspect-[320/404] max-w-xs" key={i}>
                 <ProfileCard rank={i + 1} />
               </CarouselItem>
             ))}
@@ -145,7 +145,7 @@ function TopMusicians() {
         <div className="container">
           <Button className="w-full gap-4" variant="outline" asChild>
             <Link href="/charts">
-              <BsBarChart className="w-[13px] h-[13px]" />
+              <BsBarChart className="h-[13px] w-[13px]" />
               View Charts
             </Link>
           </Button>
