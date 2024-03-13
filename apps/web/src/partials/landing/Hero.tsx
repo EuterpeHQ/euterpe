04581a8757cfd3655daa1a416cf37e6d63d49f78
Tolu Olugbesan <tolugbesan@gmail.com>
2024-03-13 12:52:43 +0100

    Add link from landing page to app

--- apps/web/src/partials/landing/Hero.tsx
+++ apps/web/src/partials/landing/Hero.tsx
@@ -35,7 +35,7 @@ function Hero() {
             their journey and share in their achievements.
           </p>
           <Button className="w-full gap-4 md:w-fit" asChild>
-            <Link href="/app" target="_blank" rel="noopener noreferrer">
+            <Link href="/discover" target="_blank" rel="noopener noreferrer">
               <HiOutlineRocketLaunch className="h-4 w-4" />
               Launch App
             </Link>
