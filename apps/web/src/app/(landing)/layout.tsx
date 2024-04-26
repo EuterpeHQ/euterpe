970006e9999bcf5d58e8748ea29fb25392fdc3a2
Tolu Olugbesan <tolugbesan@gmail.com>
2024-04-26 09:14:16 +0100

    Add announcement

--- apps/web/src/app/(landing)/layout.tsx
+++ apps/web/src/app/(landing)/layout.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Footer from "@/partials/Footer";
 import Spacer from "@/components/ui/spacer";
 import { Logo } from "@/components/Logo";
+import AnnouncementBar from "@/components/AnnouncementBar";
+import Link from "next/link";
 
 function Layout({
   children,
@@ -10,6 +12,19 @@ function Layout({
 }>) {
   return (
     <>
+      <AnnouncementBar>
+        <div>
+          <p className="text-sm">
+            Support Euterpe at &nbsp;
+            <Link
+              href="https://explorer.gitcoin.co/#/round/42161/23/51"
+              className="text-primary"
+            >
+              GG20 🎉🎉
+            </Link>
+          </p>
+        </div>
+      </AnnouncementBar>
       <Spacer size={40} />
       <div className="w-full text-center">
         <Logo />
