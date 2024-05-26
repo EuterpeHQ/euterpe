d1a256d0fafe9aa0d863cfc281d593c292ea4855
Obua Stella <126823747+obuastella@users.noreply.github.com>
2024-05-26 16:04:49 +0000

    harmonies and profile page

--- apps/web/src/app/(auth)/(creator)/harmonies/components/CollectionModal.tsx
+++ apps/web/src/app/(auth)/(creator)/harmonies/components/CollectionModal.tsx
@@ -10,7 +10,7 @@ interface CollectionModalProps {
 const CollectionModal: React.FC<CollectionModalProps> = ({ index, onClose }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="relative w-full max-w-xl p-6 bg-white rounded-xl">
+      <div className="bg-card relative w-[700px]  p-6  rounded-xl">
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
@@ -30,27 +30,35 @@ const CollectionModal: React.FC<CollectionModalProps> = ({ index, onClose }) => 
             />
           </svg>
         </button>
-        <div className="mt-4">
-          {/* <Image
-            src="https://github.com/mwororokevin/nft-preview-card-component/blob/master/images/image-avatar.png?raw=true"
-            className="rounded-xl object-cover object-bottom"
-            alt=""
-            width={400}
-            height={400}
-          /> */}
+        <div className="">
+          <section className='flex justify-between  items-center'>
+
+          <div className='mt-4 w-[50%] h-[350px]'>
          <Image
             src={HarmonyImage}
             className="h-full w-full cursor-pointer rounded-xl object-cover object-bottom transition duration-200 hover:scale-105 hover:opacity-80"
-            alt=""
+            alt="harmony image"
             aria-hidden="true"
-            fill
+            width={300}
+            height={200}
             placeholder="blur"
             quality={100}
-            width={400}
-            height={400}
           />
-          <div className="mt-4 text-gray-800">
-            <h3 className="text-lg">Ensemble #{29 + index}</h3>
+          </div>
+          <aside className='mt-8 rounded-md w-[48%] p-2'>
+            <p className='text-primary font-bold text-lg'>Ensemble #{29 + index} Harmony</p>
+            <p>Discover a unique musical experience with this exclusive harmony. 
+            <br/>
+             
+              This piece blends soothing melodies and intricate arrangements, capturing the essence of artistry. 
+              <br/>
+              <br/>
+              Claim it now and immerse yourself in a captivating soundscape.</p>
+              <button className='mt-4 p-2 rounded-lg bg-primary text-black font-semibold'>Claim Harmony</button>
+          </aside>
+            </section>
+          <div className="mt-4 text-white">
+            <h3 className="text-lg text-white">Ensemble #{29 + index}</h3>
             <p className="mt-2">A 5% chance to earn 1% of every 1000 streams on our Pan African Rockstar Album.</p>
             <p className="mt-2">ETP: 0.041</p>
           </div>
