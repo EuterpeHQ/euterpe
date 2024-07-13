54a2b4e806836ef37f6efe19d4e9602f42cde4a0
Tolu Olugbesan <tollugbesan@gmail.com>
2024-07-13 18:50:52 +0000

    Update api url env var

--- apps/website/src/hooks/useFeaturedArtist.ts
+++ apps/website/src/hooks/useFeaturedArtist.ts
@@ -6,12 +6,7 @@ type ArtistResponse = {
   artists: Artist[];
 };
 
-const apiUrl =
-  process.env.NEXT_PUBLIC_APP_ENV === "production"
-    ? process.env.NEXT_PUBLIC_API_URL
-    : process.env.NEXT_PUBLIC_APP_ENV === "staging"
-      ? process.env.NEXT_PUBLIC_STAGING_API_URL
-      : process.env.NEXT_PUBLIC_LOCAL_DEV_API_URL;
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 async function fetchFeaturedArtists() {
   const response = await axios.get<{
